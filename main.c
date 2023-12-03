67de92ae53e88d43f75c501b0f933433f40ca831
Shehab Hesham <145757675+Shehab-Hesham@users.noreply.github.com>
2023-12-03 14:33:05 +0200

    main template with all drivers' included
    Remove paths to unnecessary drivers.
    HAL: Hardware Abstraction Layer.
    MCAL: Microcontroller Abstraction Layer.

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 14, 2023
+ *      Author: SHAHB
+ */
+
+#include "util/delay.h"
+#include "Drivers/STD.h"
+#include "Drivers/BITMATH.h"
+#include "Drivers/MCAL/DIO/DIO_AD.h"
+#include "Drivers/MCAL/Interrupts/GLOBAL_INTERRUPT.h"
+#include "Drivers/MCAL/Interrupts/EXT_INTERRUPT.h"
+#include "Drivers/MCAL/ADC/ADC_INIT.h"
+#include "Drivers/MCAL/Timer/TIMER0_INT.h"
+#include "Drivers/MCAL/Timer/MCU_HW_Timer1.h"
+#include "Drivers/MCAL/UART/UART_CINFIG.h"
+#include "Drivers/MCAL/UART/UART_PRIV.h"
+#include "Drivers/MCAL/UART/PROTOTYPES.h"
+#include "Drivers/MCAL/SPI/SPI_HW.h"
+#include "Drivers/MCAL/I2C/I2C_HW.h"
+#include "Drivers/HAL/EEPROM/PROTOTYPES.h"
+#include "Drivers/HAL/Keypad/KEYPAD_INIT.h"
+#include "Drivers/HAL/LCD/LCD_Init.h"
+
+void main ()
+{
+	while (1)
+	{
+
+	}
+}

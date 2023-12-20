806ec9b2762e9adc127fb37ac2fc46f5c1088c91
Shehab Hesham <145757675+Shehab-Hesham@users.noreply.github.com>
2023-12-20 14:23:14 +0200

    EEPROM Prototypes

---
+++ Drivers/HAL/EEPROM/PROTOTYPES.h
@@ -0,0 +1,13 @@
+/*
+ * PROTOTYPES.h
+ *
+ *  Created on: Nov 17, 2023
+ *      Author: SHAHB
+ */
+
+#ifndef DRIVERS_MCAL_EEPROM_PROTOTYPES_H_
+#define DRIVERS_MCAL_EEPROM_PROTOTYPES_H_
+
+
+
+#endif /* DRIVERS_MCAL_EEPROM_PROTOTYPES_H_ */
